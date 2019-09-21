6ecc0e6e2dc417489b2f691225516df061faf91a
ardi <arizmaldi@gmail.com>
2019-09-21 22:48:21 +0700

    Create state current user for sign out

--- src/App.js
+++ src/App.js
@@ -7,11 +7,36 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SingInAndSignUpPage from './pages/signIn-and-signUp/signIn-and-signUp.component';
+import { auth } from './components/firebase/firebase.utils';
 
-function App() {
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscibeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscibeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user});
+
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscibeFromAuth();
+  }
+
+  render() {
   return (
     <div>
-      <Header/>
+      <Header currentUser={this.state.currentUser} />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
@@ -19,6 +44,7 @@ function App() {
       </Switch>
     </div>
   );
+  }
 }
 
 export default App;
