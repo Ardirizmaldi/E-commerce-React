70c4ff731b724b2fc881fabad256cc3459c829fc
ardi <arizmaldi@gmail.com>
2019-09-21 14:46:51 +0700

    SignUp and signIn component

---
+++ src/pages/signIn-and-signUp/signIn-and-signUp.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './signIn-and-signUp.component.styles.scss';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const SingInAndSignUpPage = () => (
+    <div className='signIn-and-signUp'>
+        <SignIn/>
+    </div>
+);
+
+export default SingInAndSignUpPage;
