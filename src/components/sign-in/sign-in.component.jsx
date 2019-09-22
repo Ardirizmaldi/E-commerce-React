4888bb8e57a2c539febc380a6a6e7b624eb5264c
ardi <arizmaldi@gmail.com>
2019-09-22 14:23:58 +0700

    Adding sign up component and connecting to form  sign in (Issue styling sign up)

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -10,15 +10,15 @@ class SignIn extends React.Component {
     super(props);
 
     this.state = {
-      email: "",
-      password: ""
+      email: '',
+      password: ''
     };
   }
 
   handleSubmit = event => {
     event.preventDefault();
 
-    this.setState({ email: "", password: "" });
+    this.setState({ email: '', password: '' });
   };
 
   handleChange = event => {
@@ -29,34 +29,32 @@ class SignIn extends React.Component {
 
   render() {
     return (
-      <div className="sign-in">
+      <div className='sign-in'>
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
           <FormInput
-            name="email"
-            type="email"
-            value={this.state.email}
+            name='email'
+            type='email'
             handleChange={this.handleChange}
-            label="email"
+            value={this.state.email}
+            label='email'
             required
           />
-
           <FormInput
-            name="password"
-            type="password"
+            name='password'
+            type='password'
             value={this.state.password}
             handleChange={this.handleChange}
-            label="password"
+            label='password'
             required
           />
           <div className='buttons'>
-          <CustomButton type="submit">SIGN IN</CustomButton>
-          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-            {''}
-            Sign in with Google{""}
-          </CustomButton>
+            <CustomButton type='submit'> Sign in </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
           </div>
         </form>
       </div>
--- src/pages/signIn-and-signUp/signIn-and-signUp.component.jsx
+++ src/pages/signIn-and-signUp/signIn-and-signUp.component.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './signIn-and-signUp.component.styles.scss';
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 const SingInAndSignUpPage = () => (
     <div className='signIn-and-signUp'>
         <SignIn/>
+        <SignUp/>
     </div>
 );
 
