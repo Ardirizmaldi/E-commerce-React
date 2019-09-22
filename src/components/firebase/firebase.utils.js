06d33129ca9413fbcc304718d1d62d8e55baa1f6
ardi <arizmaldi@gmail.com>
2019-09-22 14:22:57 +0700

    Adding database from firestore config

--- src/components/firebase/firebase.utils.js
+++ src/components/firebase/firebase.utils.js
@@ -1,4 +1,4 @@
-import firebase from 'firebase';
+import * as firebase from 'firebase/app';
 
 import 'firebase/firestore';
 import 'firebase/auth';
@@ -12,6 +12,32 @@ const config = {
     messagingSenderId: "562117182090",
     appId: "1:562117182090:web:71053b3cab19ff025a2373"
 };
+
+export const createUserProfileDocument = async(userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot =  await userRef.get();
+
+    if(!snapShot.exists) {
+        const {displayName, email} = userAuth;
+        const createAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('Error creating user', error.message);
+        }
+    }
+
+    return userRef;
+}
 
 firebase.initializeApp(config);
 export const auth = firebase.auth();
