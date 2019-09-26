623ca7a9d216a4ea6f657631181597a52400cf0d
ardi <arizmaldi@gmail.com>
2019-09-26 22:58:35 +0700

    1. Adding selector for user redux state 2. Adding checkout pages router

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ import {
   createUserProfileDocument
 } from "./components/firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.action";
+import { createStructuredSelector } from "reselect";
+import { selectCurrentUser } from "./redux/user/user.selector";
+import CheckoutPage from "./pages/checkout/checkout.component";
 
 class App extends React.Component {
   unsubscibeFromAuth = null;
@@ -44,6 +47,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage}/>
           <Route
             exact
             path="/signin"
@@ -61,8 +65,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser : selectCurrentUser,
 });
 
 const mapDispatchToProps = dispatch => ({
