37065d63fc2b2a8a8f4a51f6aed3675545732ba1
ardi <arizmaldi@gmail.com>
2019-09-26 23:47:11 +0700

    Added checkout items displayed

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -7,6 +7,7 @@ import {
   selectCartItems,
   selectCartTotal
 } from "../../redux/cart/cart.selectors";
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -27,9 +28,10 @@ const CheckoutPage = ({ cartItems, total }) => (
         <span>Remove</span>
       </div>
     </div>
-    {cartItems.map(cartItems => cartItems.name)}
-    <div className='total'>
-        <span>TOTAL: ${total}</span>
+    {cartItems.map(cartItem => 
+    <CheckoutItem key={cartItem.id} cartItem={cartItem} /> )}
+    <div className="total">
+      <span>TOTAL: ${total}</span>
     </div>
   </div>
 );
