cfb7f9d628602762af1e4754786bf269f25992ac
ardi <arizmaldi@gmail.com>
2019-09-29 21:02:31 +0700

    Convert Option Div component to OptionLink styles

--- src/components/header/header.styles.jsx
+++ src/components/header/header.styles.jsx
@@ -1,10 +1,11 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
+// import styled, { css } from "styled-components";
 import { Link } from "react-router-dom";
 
-const OptionsContainerStyles = css`
-  padding: 10px 15px;
-  cursor: pointer;
-`;
+// const OptionsContainerStyles = css`
+//   padding: 10px 15px;
+//   cursor: pointer;
+// `;
 
 export const HeaderContainer = styled.div`
   height: 70px;
@@ -29,9 +30,6 @@ export const OptionsContainer = styled.div`
 `;
 
 export const OptionLink = styled(Link)`
-  ${OptionsContainerStyles}
-`;
-
-export const OptionDiv = styled.div`
-  ${OptionsContainerStyles}
+  padding: 10px 15px;
+  cursor: pointer;
 `;
