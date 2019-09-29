3b7335da20494332cad0a522793318316e7ad2f7
ardi <arizmaldi@gmail.com>
2019-09-29 22:02:51 +0700

    HW 3 style cart item with js styled component lib

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 
 import "./cart-item.styles.scss";
+import { CartItemContainer, ItemImage, ItemDetails } from "./cart-item.styles";
 
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
-  <div className="cart-item">
-    <img src={imageUrl} alt="item" />
-    <div className="item-details">
-      <span className="name">{name}</span>
-      <span className="price">
+  <CartItemContainer>
+    <ItemImage src={imageUrl} alt="item" />
+    <ItemDetails>
+      <span>{name}</span>
+      <span>
         {quantity} x ${price}
       </span>
-    </div>
-  </div>
+    </ItemDetails>
+  </CartItemContainer>
 );
 
 export default CartItem;
