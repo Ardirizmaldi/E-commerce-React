14f3688f40e307df83cf3b3d28c0ae9b3259e17b
ardi <arizmaldi@gmail.com>
2019-10-02 21:33:44 +0700

    Adding more state redux to implement redux thunk 1.  Fetch state (Fetching or loading) 2. Fetching collection succes or failure

--- src/pages/shop/shop.action.js
+++ src/pages/shop/shop.action.js
@@ -1,6 +1,34 @@
 import ShopActionType from "./shop.types";
+import {
+  firestore,
+  convertCollectionSnapshotToMap
+} from "../../components/firebase/firebase.utils";
 
-export const updateCollections = (collectionsMap) => ({
-    type: ShopActionType.UPDATE_COLLECTIONS,
-    payload: collectionsMap
-})
+export const fetchCollectionsStart = () => ({
+  type: ShopActionType.FETCH_COLLECTION_START
+});
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+  type: ShopActionType.FETCH_COLLECTION_SUCCES,
+  payload: collectionsMap
+});
+
+export const fetchCollectionsFailure = errorMessage => ({
+  type: ShopActionType.FETCH_COLLECTION_FAILURE,
+  payload: errorMessage
+});
+
+export const fetchCollectionsStartAsnyc = () => {
+  return dispatch => {
+    const collectionRef = firestore.collection("collections");
+    dispatch(fetchCollectionsStart());
+
+    collectionRef
+      .get()
+      .then(snapshot => {
+        const collectionsMap = convertCollectionSnapshotToMap(snapshot);
+        dispatch(fetchCollectionsSuccess(collectionsMap));
+      })
+      .catch(error => dispatch(fetchCollectionsFailure(error.message)));
+  };
+};
--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,15 +1,30 @@
 import ShopActionType from "../../pages/shop/shop.types";
 
 const INITIAL_STATE = {
-    collections: null
+    collections: null,
+    isFetching: false,
+    errorMessage: undefined
+
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
     switch(action.type) {
-        case ShopActionType.UPDATE_COLLECTIONS:
+        case ShopActionType.FETCH_COLLECTION_START:
             return {
                 ...state,
+                isFetching: true
+            }
+        case ShopActionType.FETCH_COLLECTION_SUCCES:
+            return {
+                ...state,
+                isFetching: false,
                 collections: action.payload
+            }
+        case ShopActionType.FETCH_COLLECTION_FAILURE:
+            return {
+                ...state,
+                isFetching: false,
+                errorMessage: action.payload
             }
         default:
             return state;
