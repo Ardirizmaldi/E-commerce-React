cf025205253bc316e73813c24d4e6ee2e6414f3a
ardi <arizmaldi@gmail.com>
2019-10-05 22:18:02 +0700

    Implement saga in signup component

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-
+import { connect } from "react-redux";
 import "./sign-up.styles.scss";
 
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { auth, createUserProfileDocument } from "../firebase/firebase.utils";
+// import { auth, createUserProfileDocument } from "../firebase/firebase.utils";
 import { SignUpContainer, SignUpTitle } from "./sign-up.styles";
+import { signUpStart } from "../../redux/user/user.action";
 
 class SignUp extends React.Component {
   constructor() {
@@ -21,7 +22,7 @@ class SignUp extends React.Component {
 
   handleSubmit = async event => {
     event.preventDefault();
-
+    const { signUpStart } = this.props;
     const { displayName, email, password, confirmPassword } = this.state;
 
     if (password !== confirmPassword) {
@@ -29,23 +30,25 @@ class SignUp extends React.Component {
       return;
     }
 
-    try {
-      const { user } = await auth.createUserWithEmailAndPassword(
-        email,
-        password
-      );
+    signUpStart({ displayName, email, password });
+    // SUDAH MENGGUNAKAN SAGA
+    // try {
+    //   const { user } = await auth.createUserWithEmailAndPassword(
+    //     email,
+    //     password
+    //   );
 
-      await createUserProfileDocument(user, { displayName });
+    //   await createUserProfileDocument(user, { displayName });
 
-      this.setState({
-        displayName: "",
-        email: "",
-        password: "",
-        confirmPassword: ""
-      });
-    } catch (error) {
-      console.error(error);
-    }
+    //   this.setState({
+    //     displayName: "",
+    //     email: "",
+    //     password: "",
+    //     confirmPassword: ""
+    //   });
+    // } catch (error) {
+    //   console.error(error);
+    // }
   };
 
   handleChange = event => {
@@ -100,4 +103,11 @@ class SignUp extends React.Component {
   }
 }
 
-export default SignUp;
+const mapDispatchToProps = dispatch => ({
+  signUpStart: userCredentials => dispatch(signUpStart(userCredentials))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(SignUp);
