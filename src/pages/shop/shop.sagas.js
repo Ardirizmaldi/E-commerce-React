20cbe1fa2480f52cba851cf3d1b98584436b5e78
ardi <arizmaldi@gmail.com>
2019-10-05 21:22:21 +0700

    Create shop sagas state fetch collection

--- src/pages/shop/shop.sagas.js
+++ src/pages/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from "redux-saga/effects";
+import { takeLatest, call, put, all } from "redux-saga/effects";
 import ShopActionType from "./shop.types";
 import {
   firestore,
@@ -23,5 +23,12 @@ export function* fetchCollectionsAsync() {
 }
 
 export function* fetchCollectionsStart() {
-  yield takeLatest(ShopActionType.FETCH_COLLECTION_START, fetchCollectionsAsync);
+  yield takeLatest(
+    ShopActionType.FETCH_COLLECTION_START,
+    fetchCollectionsAsync
+  );
+}
+
+export function* shopSagas() {
+  yield all([call(fetchCollectionsStart)]);
 }
