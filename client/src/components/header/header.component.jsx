d35f2ebf142724db6d6aa8dbd79da773ca3fea24
ardi <arizmaldi@gmail.com>
2019-10-06 23:39:18 +0700

    Fix error when sign in header component optionLink

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -26,7 +26,7 @@ const Header = ({ currentUser, hidden, signOutStart }) => (
       <OptionLink to="/shop">SHOP</OptionLink>
       <OptionLink to='/shop'>CONTACT</OptionLink>
       {currentUser ? (
-        <OptionLink as='div' to='' onClick={signOutStart}>
+        <OptionLink to='' onClick={signOutStart}>
           SIGN OUT
         </OptionLink>
       ) : (
