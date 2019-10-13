6bd185869b0f2c2fd9b9284ad04cd58787c80b19
ardi <arizmaldi@gmail.com>
2019-10-13 15:38:01 +0700

    Adding some error boundary for display error page

---
+++ client/src/components/error-boundary/error-boundary.component.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  ErrorImageOverlay,
+  ErrorImageContainer,
+  ErrorImageText
+} from "./error-boundary.styles";
+
+class ErrorBoundary extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      hasErrored: false
+    };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Process the error
+    return { hasErrored: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.log(error);
+  }
+
+  render() {
+    if (this.state.hasErrored) {
+      return (
+        <ErrorImageOverlay>
+          <ErrorImageContainer imageUrl="https://i.imgur.com/U3vTGjX.png" />
+          <ErrorImageText>There’s a Leak in the Website</ErrorImageText>
+        </ErrorImageOverlay>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
