5d1a181e74364aac3eee361fc4ea32e22089d34b
ardi <arizmaldi@gmail.com>
2019-10-26 23:24:02 +0700

    Fix dark mode toggle styling an use react hooks for darkmode

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,10 @@ import { checkUserSession } from "./redux/user/user.action";
 import { GlobalStyle } from "./global.styles";
 import Spinner from "./components/spinner/spinner.component";
 import ErrorBoundary from "./components/error-boundary/error-boundary.component";
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from "./components/toggle/theme";
+import Toggle from "./components/toggle/toggle.component";
+import { useDarkMode } from "./components/toggle/useDarkMode";
 
 const HomePage = lazy(() => import("./pages/homepage/homepage.component"));
 const ShopPage = lazy(() => import("./pages/shop/shop.component"));
@@ -22,26 +26,36 @@ const App = ({ checkUserSession, currentUser }) => {
     checkUserSession();
   }, [checkUserSession]);
 
+  const [theme, toggleTheme] = useDarkMode();
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
+
   return (
     <div>
-      <GlobalStyle />
-      <Header />
-      <Switch>
-        <ErrorBoundary>
-          <Suspense fallback={<Spinner />}>
-            <Route exact path="/" component={HomePage} />
-            <Route path="/shop" component={ShopPage} />
-            <Route exact path="/checkout" component={CheckoutPage} />
-            <Route
-              exact
-              path="/signin"
-              render={() =>
-                currentUser ? <Redirect to="/" /> : <SingInAndSignUpPage />
-              }
-            />
-          </Suspense>
-        </ErrorBoundary>
-      </Switch>
+      <ThemeProvider theme={themeMode}>
+        <GlobalStyle />
+        <Header />
+        <Switch>
+          <ErrorBoundary>
+            <Suspense fallback={<Spinner />}>
+              <Route exact path="/" component={HomePage} />
+              <Route path="/shop" component={ShopPage} />
+              <Route exact path="/checkout" component={CheckoutPage} />
+              <Route
+                exact
+                path="/signin"
+                render={() =>
+                  currentUser ? <Redirect to="/" /> : <SingInAndSignUpPage />
+                }
+              />
+            </Suspense>
+            <Toggle class="mode" theme={theme} toggleTheme={toggleTheme} />
+            <div className="mode">
+            <small><b>icon</b> from <a href="https://www.flaticon.com">www.flaticon.com</a></small>
+            </div>
+          </ErrorBoundary>
+        </Switch>
+      </ThemeProvider>
     </div>
   );
 };
